89a90575ebbd8b353de0f06d79192d50b0e02973
markusarnoldsson <62673521+markusarnoldsson@users.noreply.github.com>
2021-09-09 16:47:59 +0200

    Started work on minimax
    Started work on minimax
    
    Co-authored-by: Tim Carlsson<62722541+cati19hd@users.noreply.github.com>
    Co-authored-by: Markus Arnoldsson <62673521+markusarnoldsson@users.noreply.github.com>

--- FSAI/Minimax.fs
+++ FSAI/Minimax.fs
@@ -1,8 +1,24 @@
 ﻿namespace FSAI
 
 module Minimax =
-    let printHello message = 
-        $"Hello, world {message}"
+    
+    //Temp functions
+    let eval state = 10000
+    let getWinner state = 0
+    let getValidMoves state tile = [(1,2); (2,2); (3,2)]
+
+    let rec MiniMaxAlphaBeta state depth a b tile isMaxPlayer =
+        
+        //Return Evaluation depending on state
+        if depth = 0 || (getWinner state) = 0 then
+            eval state
+        else
+            let validMoves = getValidMoves state tile
+            let bestScore = 0
+            if validMoves <> [] then
+                //FOR EACH
+                0
+            else MiniMaxAlphaBeta state depth a b tile isMaxPlayer
 
     type Class1() = 
         member this.X = "F#"

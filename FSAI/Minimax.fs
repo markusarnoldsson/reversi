bdcdeacd67d1a1c448e6fc2f1a6f78da29e66df5
markusarnoldsson <62673521+markusarnoldsson@users.noreply.github.com>
2021-09-21 16:30:57 +0200

    Changed MiniMax implementation making moveAnalysis nested
    Changed MiniMax implementation making moveAnalysis nested into the minimax function. Also did a logic change in moveAnalysis, making it use minimax function recursively.

--- FSAI/Minimax.fs
+++ FSAI/Minimax.fs
@@ -29,53 +29,51 @@ module Minimax =
     let applyMakeMove makeMove (firstValue:byte[,]) (secondValue:(int*int)) (thirdValue:byte) = // Tre inputs
         () |> ignore
 
+    // Minimax-algorithm med alpha-beta klippning
 
-    let rec moveAnalysis state (validMoves: (int*int)list) tile depth a b isMaxPlayer (bestScore) =
-        if depth = 0 then bestScore
-        else 
-            if b <= a then bestScore
-            else
-                let childBoard = state
-                let nodeScore = moveAnalysis childBoard validMoves (OtherTile(tile)) (depth-1) a b (not isMaxPlayer) bestScore
+    let rec MiniMaxAlphaBeta state depth a b tile isMaxPlayer =
 
-                MakeMove childBoard validMoves.Head tile |> ignore
-                if isMaxPlayer = true then
-                    let bestScore =
-                        if bestScore < nodeScore then
-                            nodeScore
-                        else
-                            bestScore
-                    let a =
-                        if bestScore < a then
-                            a
-                        else bestScore
-                    ()
+        //en rec funktion som sköter alpha-beta klippningen i stället för for-loopen
+        let rec moveAnalysis (state: byte[,]) (validMoves: (int*int)list) (tile: byte) (a: int) (b: int) (isMaxPleyer: bool) (bestScore: int) =
+            match validMoves with
+            | [] -> bestScore
+            | head::tail ->
+
+                let childState = applyMakeMove state head tile 
+                let nodeScore = MiniMaxAlphaBeta childState (depth-1) a b (OtherTile tile) (not isMaxPlayer)
+                if isMaxPlayer then
+                    let maxScore = max bestScore nodeScore
+                    let nextA = max bestScore a
+
+                    if b <= nextA then
+                        maxScore //Basfall för den rekursiva funktionen
+                    else
+                        (moveAnalysis state tail tile nextA b isMaxPlayer maxScore)
+
                 else
-                    let bestScore =
-                        if bestScore < nodeScore then
-                            nodeScore
-                        else
-                            bestScore
-                    let b =
-                        if bestScore < b then
-                            b
-                        else bestScore
-                    ()
-                moveAnalysis childBoard validMoves.Tail (OtherTile(tile)) depth a b (not isMaxPlayer) bestScore
-             
+                    let minScore = min bestScore nodeScore
+                    let nextB = min bestScore b
 
-    let rec MiniMaxAlphaBeta state depth a b tile isMaxPlayer  =
-        
-        //Return Evaluation depending on state
-        if depth = 0 || (getWinner state) = 0 then
-            eval state
+                    if nextB <= a then
+                        minScore //Basfall för den rekursiva funktionen
+                    else
+                        (moveAnalysis state tail tile a nextB isMaxPlayer minScore)
+
+        if depth = 0 || (applyGetWinner state <> empty) then
+            (applyEvaluation state)
         else
-            let bestScore = 0
-            let validMoves = getValidMoves state tile
-            if validMoves <> [] then
-                moveAnalysis state validMoves tile depth a b isMaxPlayer bestScore
-            else MiniMaxAlphaBeta state depth a b tile isMaxPlayer
+            let bestScore = 
+                match isMaxPlayer with
+                | true -> System.Int32.MinValue
+                | false -> System.Int32.MaxValue
+            let validMoves = applyGetValidMoves state tile
 
+            //titta ifall det finns några möjliga drag, isf fortsätt
+            //annars byt spelare
+            if validMoves.IsEmpty then
+                (MiniMaxAlphaBeta state depth a b (applyOtherTile tile) (not isMaxPlayer))
+            else
+                (moveAnalysis state validMoves tile a b isMaxPlayer bestScore)
     type Class1() = 
         member this.X = "F#"
     
